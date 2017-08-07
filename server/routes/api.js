0c3e6369b994b9b7887a0a5b6a40ec7a9099c13c
moeabdol <mohd.a.saed@gmail.com>
2017-08-07 21:12:44 +0300

    Implement GET /videos and /videos/:id

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const Video = require("../models/video");
 
-router.get("/", (req, res) => {
-  res.send("API works");
+const db = "mongodb://localhost:27017/videos";
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db, { useMongoClient: true }, (err) => {
+  if (err) { console.log("Error! " + err); }
+});
+
+router.get("/videos", (req, res) => {
+  Video.find((err, videos) => {
+    if (err) {
+      res.send(500, err);
+    } else {
+      res.status(200).json(videos);
+    }
+  });
+});
+
+router.get("/videos/:id", (req, res) => {
+  Video.findById(req.params.id, (err, video) => {
+    if (err) {
+      res.send(404, err);
+    } else {
+      res.status(200).json(video);
+    }
+  });
 });
 
 module.exports = router;
